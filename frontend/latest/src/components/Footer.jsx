b28749e35936db64f9b88cac1df459e8dfb35cc8
Archisman <archismannc2005@gmail.com>
2025-07-09 17:53:58 +0530

    fix: force footer text and icons to white in dark mode

--- frontend/latest/src/components/Footer.jsx
+++ frontend/latest/src/components/Footer.jsx
@@ -4,24 +4,24 @@ import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
-    <footer className="fixed bottom-0 left-0 w-full bg-blue-600 dark:bg-gray-800 text-white shadow-md z-50">
+    <footer className="fixed bottom-0 left-0 w-full bg-blue-600 dark:bg-gray-800 text-white shadow-md z-50 dark:text-white">
       <div className="flex justify-around items-center py-3">
         {/* Home */}
-        <Link to="/" className="flex flex-col items-center text-sm hover:opacity-80">
-          <Home size={24} />
-          <span>Home</span>
+        <Link to="/" className="flex flex-col items-center text-sm hover:opacity-80 text-white dark:text-white">
+          <Home size={24} className="text-white dark:text-white" />
+          <span className="text-white dark:text-white">Home</span>
         </Link>
 
         {/* Track Shuttle */}
-        <Link to="/track-shuttle" className="flex flex-col items-center text-sm hover:opacity-80">
-          <MapPin size={24} />
-          <span>Track&nbsp;Shuttle</span>
+        <Link to="/track-shuttle" className="flex flex-col items-center text-sm hover:opacity-80 text-white dark:text-white">
+          <MapPin size={24} className="text-white dark:text-white" />
+          <span className="text-white dark:text-white">Track&nbsp;Shuttle</span>
         </Link>
 
         {/* Profile */}
-        <Link to="/driver" className="flex flex-col items-center text-sm hover:opacity-80">
-          <User size={24} />
-          <span>Profile</span>
+        <Link to="/driver" className="flex flex-col items-center text-sm hover:opacity-80 text-white dark:text-white">
+          <User size={24} className="text-white dark:text-white" />
+          <span className="text-white dark:text-white">Profile</span>
         </Link>
       </div>
     </footer>
